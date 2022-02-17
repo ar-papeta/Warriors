1dd8e9b50fc6b12ab09c2fc86e454a42849ba9df
Artem Papeta <ar.papeta@gmail.com>
2022-02-17 22:57:32 +0200

    Create warlord instance.

---
+++ Warriors/Warlord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warriors
+{
+    public class Warlord : Defender
+    {
+        private Army _army;
+        public Warlord()
+        {
+            Attack = 4;
+            MaxHealth = 100;
+            Health = MaxHealth;
+        }
+    }
+}
